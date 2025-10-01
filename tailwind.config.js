106555e763bae58c5285fe01a84e1e2e097df793
flatoutratout <flatoutwithyaratout@gmail.com>
2025-10-01 22:27:41 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,jsx}','./components/**/*.{js,jsx}'],
-  theme: { extend: {} },
+  darkMode: "class", // <-- needed for manual toggling
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
